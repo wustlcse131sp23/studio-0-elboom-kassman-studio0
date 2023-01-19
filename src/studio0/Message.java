ba2803f73bab58e00452cf2065ad6d4100c14029
drewkassman <drewkassman@10.232.130.212>
2023-01-19 13:49:07 -0600

    We completed Studio 0!

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -1,8 +1,8 @@
 package studio0;
 
 public class Message {
-
+	
 	public static void main(String args[]) {
-		System.out.println("Say hi to your group mates!");
+		System.out.println("HOW WE DOIN BROTHA");
 	}
 }
